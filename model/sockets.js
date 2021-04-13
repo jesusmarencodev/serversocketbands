aa5690fb76869b9d2ec7dec004fccd9246146540
Jesus Marenco <jesusmarencodev@gmail.com>
2021-04-13 15:37:40 -0500

    base socket

--- model/sockets.js
+++ model/sockets.js
@@ -18,6 +18,35 @@ class Sockets {
 
       socket.emit('current-bands', this.bandList.getBands());
 
+      //Votar por la banda
+      socket.on('votar-banda', (id)=>{
+        this.bandList.increaseVotes( id );
+        //solo emite al cliente
+        //socket.emit('current-bands', this.bandList.getBands());
+
+        //emitimos a todos los clientes
+        this.io.emit('current-bands', this.bandList.getBands());
+      });
+
+      socket.on('eliminar-banda', (id)=>{
+        this.bandList.removeBand( id );
+        //emitimos a todos los clientes
+        this.io.emit('current-bands', this.bandList.getBands());
+      });
+
+      
+      socket.on('cambiar-nombre-banda', ({id, nombre:name})=>{
+        this.bandList.changeName( id, name );
+        //emitimos a todos los clientes
+        this.io.emit('current-bands', this.bandList.getBands());
+      });
+
+      socket.on('nueva-banda', ({ nombre:name})=>{
+        this.bandList.addBand( name );
+        //emitimos a todos los clientes
+        this.io.emit('current-bands', this.bandList.getBands());
+      });
+
     });
     
   }
